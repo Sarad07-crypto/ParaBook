58ee89f0f1241a7ebd317f2d1e1bc1c199d28006
Saradcr7 <saradcr7adhikari@gmail.com>
2025-06-21 22:37:49 +0545

    this is flight service update

--- Web/php/connection.php
+++ Web/php/connection.php
@@ -1,8 +1,8 @@
 <?php
 $_servername = "localhost:3307";
 $_username = "root";
-$_password = "parabook";
-$_database = "";
+$_password = "";
+$_database = "parabook";
 
 $connect = mysqli_connect($_servername, $_username, $_password, $_database);
 
