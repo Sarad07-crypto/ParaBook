8ccf389dfd02405c76d08dcb1f7bd800db87f54f
Saradcr7 <saradcr7adhikari@gmail.com>
2025-06-29 21:32:30 +0545

    this is the esewa update final

--- Web/php/google/core/controller.class.php
+++ Web/php/google/core/controller.class.php
@@ -8,7 +8,7 @@ class Connect extends PDO
             parent::__construct(
                 "mysql:host=localhost:3307;dbname=parabook",
                 'root',
-                '',
+                'parabook',
                 array(
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
