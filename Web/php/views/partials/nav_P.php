d9a39e7d3b026e8b1465b6fb0bb7721ee829044b
Karun9797 <karunkarun9797@gmail.com>
2025-06-30 17:37:12 +0545

    esewa payment

--- Web/php/views/partials/nav_P.php
+++ Web/php/views/partials/nav_P.php
@@ -22,9 +22,11 @@ body {
             src="<?php echo $avatar ?>">
     </div>
     <div class="sidebar-section sidebar-links">
-        <a href="/home">Home</a>
-        <a href="/bookingcheck">Bookings</a>
-        <a href="#">Statistics</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Flight Management</a>
+        <a href="#">Bookings</a>
+        <a href="#">Analytics</a>
+        <a href="#">Revenue</a>
     </div>
     <div class="sidebar-section">
         <a href="#" style="text-decoration: none;">Notifications</a><br>
@@ -37,7 +39,7 @@ body {
 </div>
 <!-- Navigation Bar -->
 <div class="nav-bar">
-    <a href="/home">Home</a>
+    <a href="#">Dashboard</a>
     <a href="/bookingcheck">Bookings</a>
     <a href="#">Statistics</a>
 </div>
